9f40ad298486ccdb454215ef5d12b4f7f1d71e95
Richard Fleck <rfleck98@gmail.com>
2020-05-20 11:32:24 +0000

    added rf_blink performance test

---
+++ verilog/hardware/processor/source/rf_performance/rf_blink/rf_blink.c
@@ -0,0 +1,31 @@
+int
+main(void)
+{
+	/*
+	 *	Reading from the special address 0x2000 will cause sail to
+	 *	set the value of 8 of the FPGA's pins to the byte written
+	 *	to the address. See the PCF file for how those 8 pins are
+	 *	mapped.
+	 */
+	volatile unsigned int *		debugLEDs = (unsigned int *)0x8004000;
+
+	enum
+	{
+		kSpinDelay = 400000 
+	};
+
+	
+		*debugLEDs = 0xFF;
+
+		/*
+		 *	Spin
+		 */
+		for (int j = 0; j < kSpinDelay; j++);
+
+		*debugLEDs = 0x00;
+
+		/*
+		 *	Spin
+		 */
+		for (int j = 0; j < kSpinDelay; j++);		
+	}
---
+++ verilog/hardware/processor/source/rf_performance/rf_blink/run.m
@@ -0,0 +1,5 @@
+newnode		riscv
+sizemem		65536	
+srecl		"rf_blink-sf.sr"
+run
+on
